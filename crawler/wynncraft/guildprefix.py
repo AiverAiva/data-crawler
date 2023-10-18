6952c0c02ff158799d02349f49589afeb82bd151
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2023-10-18 11:49:01 +0800

    Update guildprefix.py

--- crawler/wynncraft/guildprefix.py
+++ crawler/wynncraft/guildprefix.py
@@ -15,8 +15,9 @@ existedGuild = list(map(lambda x: prefixData[x], prefixData))
 
 for i in guildList:
     if (i in existedGuild): 
-        pass
+        print(f"{i} passed")
     else:
+        print(f"Processig {i}")
         guildData = requests.get(f'https://api.wynncraft.com/public_api.php?action=guildStats&command={i}').json()
         prefixData[guildData['prefix']] = i
         with open(path, 'w') as f:
