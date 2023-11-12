87debfeac40b4bcbf7c93acb228431109a3a5263
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2023-11-12 13:40:32 +0800

    perf: O(1) on querying and fix the dumb duplicated guild prefixes

--- crawler/wynncraft/guildprefix.py
+++ crawler/wynncraft/guildprefix.py
@@ -10,19 +10,31 @@ path = os.path.join('data', 'wynncraft', 'guildprefix.json')
 with open(path, 'r') as f:
     prefixData = json.load(f)
 
-existedGuild = list(map(lambda x: prefixData[x], prefixData))
-
+# existedGuild = list(map(lambda x: prefixData[x], prefixData))
+existedGuild = {k: v for v, k in prefixData.items()}
+duplicatedGuild = ['Mlg', 'Mortal Assassins', 'Masters of Wynn', 'The Luc Squad', 'SwagSquad', 'Britania', 'Legalthea', 'Wynnter of anar', 'Wynn Warriors', 'Ketsueki Akuma', 'Elite', 'DarkVictory', 'LegendsOfLeague', 'RPG', 'WynnGalaxys', 'Bounty of Wynn', 'WorldOfWynncraf', 'Guards of Wood', 'Thunderbolt', 'Mages of Wynn', 'Frost Clan', 'Wynncrafters', 'Gods of Death', 'Sword Art Onlin', 'Team CM', 'Swag Squad', 'Crescent', 'Knights of Blood', 'Shadows of Wynn', 'The Super Yetie', 'the assassins', 'TotallyLegal', 'Megitsune', 'RunesForWynn', 'DiamondArmy', 'GrimmMechas', 'DIXX', 'White Lotus', 'LucHacks', 'Social Assistance', 'gods of dungeon', 'dingfriesrdone', 'WynnGalaxy', 'Bakers Of Wynn', 'HealthyxApple', 'Shop', 'Knight Of Blood', 'WarriorsofWynn', 'Templar Lusitania', 'SAD Industries', 'Mythical', 'RMS', 'Ramu of Wynn', 'ImperialGold', 'TheSmartWynns', 'Stars Of Night', 'French Cube', 'WynnWalrus', 'Grimm', 'meme', 'Kartoshechka', 'team t tree', 'Moments', 'Theoricks Staff', 'The Memers', 'Darkest Descent']
 
 for i in guildList:
     if (i in existedGuild): 
-        print(f"{i} passed")
+        pass
+        # print(f"{i} passed")
+    elif (i in duplicatedGuild):
+        pass
     else:
-        print(f"Processig {i}")
+        print(f"Processing {i}")
         guildData = requests.get(f'https://api.wynncraft.com/public_api.php?action=guildStats&command={i}').json()
         prefixData[guildData['prefix']] = i
         with open(path, 'w') as f:
             json.dump(prefixData, f)
-        time.sleep(0.4)
+        # time.sleep(0.4)
 
-    
-
+# for i in guildList:
+#     if (i not in existedGuild): 
+#         try:
+#             guildData = requests.get(f'https://api.wynncraft.com/public_api.php?action=guildStats&command={i}').json()
+#             prefixData[guildData['prefix']] = i
+#             with open(path, 'w') as f:
+#                 json.dump(prefixData, f)
+#             time.sleep(0.4)
+#         except:
+#             print(f"Error happens on processing {i}")
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+import time
+import math
+
+start_time = time.time()
 import crawler.wynncraft.guildprefix
+print(f'took {math.floor(time.time() - start_time)}s to complete guild prefix crawling')
